13802fb12dde83d89eb1222b2e5a1eb6794e5c8a
Arnau García <arnau.garcia.gallego@gmail.com>
2017-07-26 14:58:55 +0200

    Spring MVC - Controllers - Redirecciones

--- src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/example3")
@@ -15,6 +16,18 @@ public class Example3Controller {
 
     private static final String FROM_VIEW = "form";
     private static final String RESULT_VIEW = "result";
+
+
+    //Froma 1 de redireccionar
+/*    @GetMapping("/")
+    public String redirect(){
+        return "redirect:/example3/showform";
+    }*/
+
+    @GetMapping("/")
+    public RedirectView redirect(){
+        return new RedirectView("/example3/showform");
+    }
 
     @GetMapping("/showform")
     public String showForm(Model model){
