a26d7b4fa2bd5001300d7942ce561a1c20d893ca
Arnau García <arnau.garcia.gallego@gmail.com>
2017-07-26 16:46:08 +0200

    Spring MVC - Controllers - Logs

--- src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -1,6 +1,8 @@
 package com.udemy.backendninja.controller;
 
 import com.udemy.backendninja.model.Person;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 @RequestMapping("/example3")
 public class Example3Controller {
+
+    private static final Log LOGGER = LogFactory.getLog(Example3Controller.class);
 
     private static final String FROM_VIEW = "form";
     private static final String RESULT_VIEW = "result";
@@ -31,14 +35,20 @@ public class Example3Controller {
 
     @GetMapping("/showform")
     public String showForm(Model model){
+        LOGGER.info("INFO TRACE");
+        LOGGER.warn("WARNING TRACE");
+        LOGGER.error("ERROR TRACE");
+        LOGGER.debug("DEBUG TRACE");
         model.addAttribute("person", new Person());
         return FROM_VIEW;
     }
 
     @PostMapping("/addperson")
     public ModelAndView addPerson(@ModelAttribute("person") Person person){
+        LOGGER.info("METHOD: 'addPerson' -- PARAMS: " + person.toString());
         ModelAndView modelAndView = new ModelAndView(RESULT_VIEW);
         modelAndView.addObject("person", person);
+        LOGGER.info("TEMPLATE: '" + RESULT_VIEW + "' -- DATA: '" + person + "'");
         return modelAndView;
     }
 
