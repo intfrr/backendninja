9da61bc3cf0fa290d186e9a047f82f423028e80e
Arnau García <arnau.garcia.gallego@gmail.com>
2017-07-27 16:43:52 +0200

    Spring MVC - Servicios y Componentes - Validaciones

--- src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -5,12 +5,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/example3")
@@ -35,20 +38,25 @@ public class Example3Controller {
 
     @GetMapping("/showform")
     public String showForm(Model model){
-        LOGGER.info("INFO TRACE");
+        /*LOGGER.info("INFO TRACE");
         LOGGER.warn("WARNING TRACE");
         LOGGER.error("ERROR TRACE");
-        LOGGER.debug("DEBUG TRACE");
+        LOGGER.debug("DEBUG TRACE");*/
         model.addAttribute("person", new Person());
         return FROM_VIEW;
     }
 
     @PostMapping("/addperson")
-    public ModelAndView addPerson(@ModelAttribute("person") Person person){
-        LOGGER.info("METHOD: 'addPerson' -- PARAMS: " + person.toString());
-        ModelAndView modelAndView = new ModelAndView(RESULT_VIEW);
-        modelAndView.addObject("person", person);
-        LOGGER.info("TEMPLATE: '" + RESULT_VIEW + "' -- DATA: '" + person + "'");
+    public ModelAndView addPerson(@Valid @ModelAttribute("person") Person person, BindingResult bindingResult){
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (bindingResult.hasErrors()){
+            modelAndView.setViewName(FROM_VIEW);
+        }else {
+            modelAndView.setViewName(RESULT_VIEW);
+            modelAndView.addObject("person", person);
+        }
+
         return modelAndView;
     }
 
