0d96e0851691cfd8f6d2726248169dccc07ded9f
Arnau García <arnau.garcia.gallego@gmail.com>
2017-08-10 18:38:44 +0200

    Spring MVC - Practica - Añadir contacto - Vista y controller

--- src/main/java/com/udemy/backendninja/controller/ContactController.java
+++ src/main/java/com/udemy/backendninja/controller/ContactController.java
@@ -1,17 +1,38 @@
 package com.udemy.backendninja.controller;
 
 import com.udemy.backendninja.constant.ViewConstant;
+import com.udemy.backendninja.model.ContactModel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/contacts")
 public class ContactController {
 
+    private static final Log LOG = LogFactory.getLog(ContactController.class);
+
     @GetMapping("/contactform")
-    private String redirectContactForm(){
+    public String redirectContactForm(Model model){
+        model.addAttribute("contactModel", new ContactModel());
         return ViewConstant.CONTACT_FORM;
     }
 
+    @GetMapping("/cancel")
+    public String cancel(){
+        return ViewConstant.CONTACTS;
+    }
+
+    @PostMapping("/addcontact")
+    public String addContact(@ModelAttribute(name = "contactModel") ContactModel contactModel,
+                             Model model){
+        model.addAttribute("result", 1);
+        LOG.info("Method: addContact() - Params: contactModel=" + contactModel.toString());
+        return ViewConstant.CONTACTS;
+    }
 }
